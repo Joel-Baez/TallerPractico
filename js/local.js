ddc1f8601d0a242407f407939405442a06020806
Joel-Baez <joebaez@uniboyaca.edu.co>
2025-09-06 12:04:18 -0500

    implmentacion del JS y de la logica del formulario

---
+++ js/local.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('tareaForm');
+const tareasUl = document.getElementById('tareas');
+const buscarInput = document.getElementById('buscar');
+const completadasSpan = document.getElementById('completadas');
+const pendientesSpan = document.getElementById('pendientes');
